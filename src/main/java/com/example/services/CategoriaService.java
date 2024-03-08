ecf515f8b99162bf10af8f1a71822b26db13bca0
Erick Peck <erickrpeck@gmail.com.br>
2024-03-08 19:19:28 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/example/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.example.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Categoria;
+import com.example.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+}

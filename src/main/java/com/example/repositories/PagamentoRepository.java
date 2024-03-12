d34ac7bc1bae20ba4a2ec30e1a2858a878f45ed6
Erick Peck <erickrpeck@gmail.com.br>
2024-03-12 16:29:04 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/example/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.example.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
